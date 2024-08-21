f6266aa1ca3922e45b1cb0c49deb976749267b48
Mateus Honorato <mateush.honorato@gmail.com>
2024-08-21 08:49:22 -0300

    updated arguments handler

--- include/args.h
+++ include/args.h
@@ -6,9 +6,12 @@
 
 using namespace std;
 
+extern bool allHeuristics;
+extern bool allInstances;
+extern bool verbose;
+extern int numSalesmen;
 extern string instanceFileName;
 extern string selectedHeuristic;
-extern bool verbose;
 
 void parseArguments(int argc, char *argv[]);
 
--- src/args.cpp
+++ src/args.cpp
@@ -3,20 +3,62 @@
 string instanceFileName;
 string selectedHeuristic;
 bool verbose = false;
+int numSalesmen;
+bool allHeuristics = false;
+bool allInstances = false;
 
 void parseArguments(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 2)
     {
-        cerr << "Usage: " << argv[0] << " <instance_file> <heuristic> [-v]" << endl;
+        cerr << "Usage: " << argv[0] << " <instance_file> <heuristic> <num_salesmen> [--all-heuristics] [--all-instances] [-v]" << endl;
         exit(EXIT_FAILURE);
     }
 
-    instanceFileName = argv[1];
-    selectedHeuristic = argv[2];
-
-    if (argc == 4 && string(argv[3]) == "-v")
+    for (int i = 1; i < argc; ++i)
     {
-        verbose = true;
+        string arg = argv[i];
+        if (arg == "-v")
+        {
+            verbose = true;
+        }
+        else if (arg == "--all-heuristics")
+        {
+            allHeuristics = true;
+        }
+        else if (arg == "--all-instances")
+        {
+            allInstances = true;
+        }
+        else if (instanceFileName.empty())
+        {
+            instanceFileName = arg;
+        }
+        else if (selectedHeuristic.empty())
+        {
+            selectedHeuristic = arg;
+        }
+        else if (numSalesmen == 0)
+        {
+            numSalesmen = atoi(arg.c_str());
+        }
+    }
+
+    if (!allHeuristics && selectedHeuristic.empty())
+    {
+        cerr << "Error: Heuristic must be specified unless --all-heuristics is used." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (!allInstances && instanceFileName.empty())
+    {
+        cerr << "Error: Instance file must be specified unless --all-instances is used." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (numSalesmen == 0)
+    {
+        cerr << "Error: Number of salesmen must be specified." << endl;
+        exit(EXIT_FAILURE);
     }
 }
