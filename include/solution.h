98e110f490ff5c13852d019b02d2e58d0f939fc7
Mateus Honorato <mateush.honorato@gmail.com>
2024-08-21 09:47:48 -0300

    improving the solution definition

--- include/solution.h
+++ include/solution.h
@@ -1,12 +1,24 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
+#include "instance.h"
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-typedef vector<vector<int>> Solution;
+typedef struct sRoute
+{
+    vector<int> cities;
+    float cost;
+} Route;
+
+typedef struct sSolution
+{
+    float totalCost;
+    vector<Route> routes;
+} Solution;
 
 void printSolution(const Solution &sol);
+void calculateCosts(Solution &sol, const Instance &inst);
 
 #endif // SOLUTION_H
--- src/solution.cpp
+++ src/solution.cpp
@@ -2,13 +2,29 @@
 
 void printSolution(const Solution &sol)
 {
-    for (const auto &subvector : sol)
+    for (const auto &route : sol.routes)
     {
         cout << "[ ";
-        for (const auto &elem : subvector)
+        for (const auto &elem : route.cities)
         {
             cout << elem << " ";
         }
-        cout << "]" << endl;
+        cout << "] -> Cost = " << route.cost << endl;
+    }
+    cout << "Total cost = " << sol.totalCost << endl;
+}
+
+void calculateCosts(Solution &sol, const Instance &inst)
+{
+    sol.totalCost = 0;
+    for (auto &route : sol.routes)
+    {
+        route.cost = 0;
+        for (int i = 0; i < route.cities.size() - 1; ++i)
+        {
+            route.cost += inst.distance[route.cities[i]][route.cities[i + 1]];
+        }
+        route.cost += inst.distance[route.cities.back()][route.cities.front()];
+        sol.totalCost += route.cost;
     }
 }
